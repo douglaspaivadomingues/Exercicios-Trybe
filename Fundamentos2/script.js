4f0cab08d4e3e38451cd263ca84b943125f32ef3
Douglas Domingues <douglaspaivadomingues@gmail.com>
2023-07-04 20:04:11 -0300

    'commit naõ sei de que'

--- Fundamentos2/script.js
+++ Fundamentos2/script.js
@@ -1,16 +1,2 @@
-
-const inputText = document.querySelector("#input-text");
-const inputCheckbox = document.querySelector("#input-checkbox");
-const hrefLink = document.querySelector("#href");
-
-const trybe = (event) => {
-    event.preventDefault();
-}
- hrefLink.addEventListener('click' ,trybe);
-
- inputText.addEventListener('keypress', (event) => {
-  const character = event.key;
-  if (character !== 'a') {
-    event.preventDefault();
-  }
-});
+const date = new Date();
+console.log(date);

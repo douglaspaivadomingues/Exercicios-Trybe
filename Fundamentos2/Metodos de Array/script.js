d994bad89883af0bdfe6bb9cbfdeb70773aa69ee
Douglas Domingues <douglaspaivadomingues@gmail.com>
2023-08-24 15:59:29 -0300

    'exercicios HOFS'

--- Fundamentos2/Metodos de Array/script.js
+++ Fundamentos2/Metodos de Array/script.js
@@ -282,6 +282,70 @@ const getStrongPokemonNames = (minLevel) => {
     }
   });
 };
-  
+
 const sortedPokemons = sortPokemons(pokemons);
-console.log(sortedPokemons);
+//console.log(sortedPokemons);
+
+const songs = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+// Adicione seu código aqui
+const songWithid = songs.find((song) => song.id === "31031685" );
+
+//console.log(songWithid);
+
+//Exercício 2
+//Você deve desenvolver uma função que realiza a busca de um contato telefônico em uma lista. 
+//Sua tarefa é implementar a função searchContact(contacts, name), 
+//que recebe uma lista de contatos e o nome do contato a ser encontrado. Se o contato for encontrado,
+//retornar um objeto com todas as informações do contato utilizando spread operator. 
+//Se o contato não for encontrado, retornar a mensagem “Contato não encontrado”.
+
+const contacts = [
+  { name: 'João', phone: '123456789' },
+  { name: 'Maria', phone: '987654321' },
+  { name: 'Pedro', phone: '456123789' },
+  { name: 'Ana', phone: '789456123' },
+  { name: 'Carlos', phone: '321654987' },
+  { name: 'Mariana', phone: '654123987' },
+  { name: 'Lucas', phone: '987321654' },
+  { name: 'Laura', phone: '123789456' },
+  { name: 'Tiago', phone: '789123456' },
+  { name: 'Beatriz', phone: '456789123' }
+];
+
+const searchContact = (contacts, name) => {
+  const search = contacts.find((contact) => contact.name === name); 
+    if(search) {
+      return  {...name};
+    } else {
+      return "Contato não encontrado";
+    }
+  };
+  
+  //Implemente a função addGender que adiciona a propriedade 
+  //“gender: “Animação”” em cada objeto do array de cartoons.
+
+  const cartoons = [
+  { title: 'Tom & Jerry', ageRating: 'Livre' },
+  { title: 'Scooby-Doo', ageRating: 'Livre' },
+  { title: 'Looney Tunes', ageRating: 'Livre' },
+  { title: 'The Simpsons', ageRating: 12 },
+  { title: 'Avatar: The Last Airbender', ageRating: 'Livre' },
+  { title: 'Pokémon', ageRating: 'Livre' },
+  { title: 'Dragon Ball Z', ageRating: 12 },
+  { title: 'Adventure Time', ageRating: 'Livre' },
+  { title: 'Gravity Falls', ageRating: 'Livre' },
+  { title: 'Teen Titans Go!', ageRating: 'Livre' }
+];
+
+const addGender = () => {
+  cartoons.forEach((cartoon)=> cartoon.gender = "Animation");
+  return cartoons;
+};
+
+console.log(addGender());
+
